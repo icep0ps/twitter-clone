f01576d4a24001efa9db0b68d3eb521304bf04ef
icep0ps <tapsmuko@gmail.com>
2022-12-15 12:19:26 +0200

    replies support images

--- src/pages/Tweet/components/tweet/Reply.jsx
+++ src/pages/Tweet/components/tweet/Reply.jsx
@@ -1,20 +1,57 @@
 import uniqid from 'uniqid';
 import React, { useState, useContext } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
-import { db } from '../../../../firebase/firebase-config';
+import { db, storage } from '../../../../firebase/firebase-config';
 import { doc, setDoc, Timestamp } from 'firebase/firestore';
 import { UserContext } from '../../../../Context/UserContext';
 import { COMMENT } from '../../../../common/helpers/types';
 
+import { uploadBytes, getDownloadURL, ref } from 'firebase/storage';
+
 function Reply({ username, author, id }) {
   const { user } = useContext(UserContext);
   const [tweetInput, setTweetInput] = useState('');
+  const [imagePreviewURL, setImagePreviewURL] = useState([]);
 
-  const handleTweetInput = (event) => {
-    setTweetInput(event.target.value);
-  };
+  function setImages(tweetId) {
+    const imagesURL = [];
+    const images = new Promise((resolve, reject) => {
+      if (imagePreviewURL.length === 0) {
+        return resolve(imagesURL);
+      }
 
-  const tweet = async () => {
+      imagePreviewURL.forEach(async (image, index, array) => {
+        const imageUrl = await uploadImage(tweetId, image.image);
+        imagesURL.push(imageUrl);
+        if (index === array.length - 1) {
+          resolve(imagesURL);
+        }
+      });
+    });
+
+    return images;
+  }
+
+  async function uploadImage(tweetId, image) {
+    const imageRef = ref(
+      storage,
+      `${user.displayName}/tweets/${tweetId}/images/${image.name + uniqid()}`
+    );
+
+    await uploadBytes(imageRef, image);
+    const url = await getDownloadURL(imageRef);
+    return url;
+  }
+
+  function previewImage(event) {
+    const image = event.target.files[0];
+    setImagePreviewURL((prevState) => [
+      ...prevState,
+      { image: image, url: URL.createObjectURL(image) },
+    ]);
+  }
+  const tweet = async (event) => {
+    event.preventDefault();
     const TWEET_ID = uniqid();
     const replyRef = doc(
       db,
@@ -40,23 +77,31 @@ function Reply({ username, author, id }) {
       orignalPost: id,
     });
 
-    await setDoc(tweetCommentsRef, {
-      id: TWEET_ID,
-      replyingTo: author,
-      type: COMMENT,
-      profileURL: user.photoURL,
-      username: user.displayName,
-      author: user.displayName,
-      tweet: tweetInput,
-      parentDocId: id,
-      likes: [],
-      retweets: [],
-      date: Timestamp.now(),
+    setImages(TWEET_ID).then(async (responseImages) => {
+      await setDoc(tweetCommentsRef, {
+        id: TWEET_ID,
+        replyingTo: author,
+        type: COMMENT,
+        profileURL: user.photoURL,
+        username: user.displayName,
+        author: user.displayName,
+        tweet: tweetInput,
+        parentDocId: id,
+        likes: [],
+        retweets: [],
+        images: responseImages,
+        date: Timestamp.now(),
+      });
     });
+    setTweetInput('');
+    setImagePreviewURL([]);
   };
 
   return (
-    <div className="flex flex-col relative px-5 pb ">
+    <form
+      className="flex flex-col relative px-5 pb "
+      onSubmit={(event) => tweet(event)}
+    >
       <div className="flex gap-5 items-start ">
         <div className="min-w-[48px] min-h-[48px]  bg-black rounded-3xl"></div>
         <div className="w-full">
@@ -68,17 +113,25 @@ function Reply({ username, author, id }) {
             type="text"
             placeholder="Tweet Your Reply?"
             className="resize-none text-xl grow outline-none py-3"
-            onChange={() => handleTweetInput()}
+            onChange={(event) => setTweetInput(event.target.value)}
           />
         </div>
       </div>
+      <div className="">
+        {imagePreviewURL.map((image) => {
+          return <img alt="" src={`${image.url}`} />;
+        })}
+      </div>
+      <div>
+        <input type={'file'} onChange={(event) => previewImage(event)}></input>
+      </div>
       <button
         className="p-2.5 bg-blue-500 text-white rounded-3xl w-20 self-end"
-        onClick={() => tweet()}
+        type="submit"
       >
         Tweet
       </button>
-    </div>
+    </form>
   );
 }
 
