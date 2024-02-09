aae873495649caa2e7e8556cd4ddc3d1e941f436
icep0ps <tapsmuko@gmail.com>
2024-02-09 23:15:52 +0200

    migrating to react-router v6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { AppProvider } from './Context/AppContext';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { loader } from './pages/home/Home';
+
+import Home from './pages/home/Home';
+import Signup from './pages/sign-up/Signup';
+import SignIn from './pages/sign-in/SignIn';
+import Profile from './pages/profile/Profile';
+import TweetStatus from './pages/Tweet/TweetStatus';
+import Likes from './pages/Tweet/components/tweet/Likes';
+import Retweets from './pages/Tweet/components/tweet/Retweets';
+import EditProfile from './pages/profile/components/EditProfile';
+import FollowersAndFollowing from './pages/profile/components/followersAndFollowing/FollowersAndFollowing';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    loader: loader,
+  },
+  {
+    path: '/auth/',
+    children: [
+      {
+        path: 'sign-up',
+        element: <Signup />,
+      },
+      {
+        path: 'sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
+  {
+    path: '/:author/status/:id',
+    element: <TweetStatus />,
+    children: [
+      {
+        path: 'likes',
+        element: <Likes />,
+      },
+      {
+        path: 'retweets',
+        element: <Retweets />,
+      },
+    ],
+  },
+  {
+    path: '/profile/:id',
+    element: <Profile />,
+    children: [
+      {
+        path: 'settings',
+        element: <EditProfile />,
+      },
+      {
+        path: 'followers',
+        element: <FollowersAndFollowing view={'followers'} />,
+      },
+      {
+        path: 'following',
+        element: <FollowersAndFollowing view={'following'} />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </AppProvider>
 );
